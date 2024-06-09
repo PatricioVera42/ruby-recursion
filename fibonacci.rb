313bb01d59e7ea9c1936aa0e9f70ae7ba921c748
patovera <verapatricio9@gmail.com>
2024-06-09 19:29:30 -0300

    finished fibonacci

--- fibonacci.rb
+++ fibonacci.rb
@@ -6,7 +6,16 @@ def fibs(number)
   result
 end
 
+def fibs_rec(number, arr = [0,1])
+  return arr if number == arr.length
+
+  index = arr.length
+  first = arr[-1]
+  second = arr[-2]
+  fibs_rec(number, arr.push(first + second))
+end
 
 
 
 p fibs(8)
+p fibs_rec(8)
